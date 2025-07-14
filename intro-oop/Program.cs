54a5af1f7637276239af716c4ffe83d59ce2c52f
Damon German <49420596+dgerman3@users.noreply.github.com>
2025-07-14 14:03:26 -0500

    Initial commit for branch 4.0-car-engine

--- intro-oop/Program.cs
+++ intro-oop/Program.cs
@@ -4,31 +4,7 @@ internal static class Program
 {
     private static void Main()
     {
-        var myEBook = new EBook();
-        myEBook.Download();
-        myEBook.Read();
-    }
-}
-
-public class Book
-{
-    public void Read()
-    {
-        Console.WriteLine("Reading...");
-    }
-}
-
-public class EBook
-{
-    private readonly Book _book = new();
-
-    public void Read()
-    {
-        _book.Read();
-    }
-
-    public void Download()
-    {
-        Console.WriteLine("Downloading...");
+        var myCar = new Car();
+        myCar.Start();
     }
 }
