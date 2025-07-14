f2235219b4d5ffd2c914778a81e775452299ed72
Damon German <49420596+dgerman3@users.noreply.github.com>
2025-07-14 14:03:22 -0500

    Initial commit for branch 1.9-geomety-add-square-inclass-solution

--- intro-oop/Shape.cs
+++ intro-oop/Shape.cs
@@ -4,7 +4,7 @@ public abstract class Shape : INameable
 {
     public abstract double Area();
     public Location? Location { get; set; }
-    public string GetName() // note: no override keyword
+    public string GetName()
     {
         return GetType().Name;
     }
