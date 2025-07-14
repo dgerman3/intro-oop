0b55de03dfd794b46a16779da4409ac1c3b5d131
Damon German <49420596+dgerman3@users.noreply.github.com>
2025-07-14 14:03:21 -0500

    Initial commit for branch 1.7-geomety-interfaces

--- intro-oop/Program.cs
+++ intro-oop/Program.cs
@@ -7,16 +7,35 @@ internal static class Program
         var circle = new Circle(1.0);
         var triangle = new Triangle(1.0, 2.0);
 
-        var shapes = new Shape[] { circle, triangle };
+        var shapes = new List<Shape>
+        {
+            circle,
+            triangle
+        };
+        var names = new List<INameable>
+        {
+            circle,
+            triangle,
+            new Dog()
+        };
 
         PrintArea(shapes);
+        PrintName(names);
     }
-    
-    private static void PrintArea(Shape[] shapes)
+
+    private static void PrintArea(IEnumerable<Shape> shapes)
     {
         foreach (var shape in shapes)
         {
-            Console.WriteLine(shape.Area());
+            Console.WriteLine($"Area of {shape.GetName()} = {shape.Area()}");
+        }
+    }
+
+    private static void PrintName(IEnumerable<INameable> nameables)
+    {
+        foreach (var nameable in nameables)
+        {
+            Console.WriteLine(nameable.GetName());
         }
     }
 }
