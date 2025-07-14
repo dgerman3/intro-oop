f2b02b533c734f0e0da336d0cb803108bf65681d
Damon German <49420596+dgerman3@users.noreply.github.com>
2025-07-14 14:03:30 -0500

    Initial commit for branch 6.0-smartphone-decorator-lab

--- intro-oop/Program.cs
+++ intro-oop/Program.cs
@@ -4,13 +4,11 @@ internal static class Program
 {
     private static void Main()
     {
+        ICamera camera = new Camera();
         IBattery battery = new Battery();
-        IBattery overchargeProtectedBattery = new OverchargeProtectionDecorator(battery);
 
-        var phone = new Smartphone(new Camera(), overchargeProtectedBattery);
-        for (var i = 0; i < 10; i++) // Simulating multiple charging attempts
-        {
-            phone.Charge();
-        }
+        var phone = new Smartphone(camera, battery);
+        phone.TakePhoto();
+        phone.Charge();
     }
 }
