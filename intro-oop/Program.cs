8f5ddaca04462ebbb4290fbc6660042e7aa6a0d6
Damon German <49420596+dgerman3@users.noreply.github.com>
2025-07-14 14:03:20 -0500

    Initial commit for branch 1.6-geometry-polymorphism

--- intro-oop/Program.cs
+++ intro-oop/Program.cs
@@ -4,11 +4,19 @@ internal static class Program
 {
     private static void Main()
     {
-        Console.WriteLine(new Circle(1.0).Area());
-        Console.WriteLine(new Triangle(1.0, 2.0).Area());
+        var circle = new Circle(1.0);
+        var triangle = new Triangle(1.0, 2.0);
 
-        var location1 = new Location { X = 1.0, Y = 2.0 };
-        var location2 = new Location { X = 1.0, Y = 2.0 };
-        Console.WriteLine(location1.Equals(location2));
+        var shapes = new Shape[] { circle, triangle };
+
+        PrintArea(shapes);
+    }
+    
+    private static void PrintArea(Shape[] shapes)
+    {
+        foreach (var shape in shapes)
+        {
+            Console.WriteLine(shape.Area());
+        }
     }
 }
